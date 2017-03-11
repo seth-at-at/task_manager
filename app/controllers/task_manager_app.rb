07987ec6571d72daf36c5e65cfd70f52b26af070
Seth <seth19483@gmail.com>
2017-03-10 17:58:44 -0700

    confused about a couple things

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,28 @@
+require_relative '../models/task.rb'
+
+class TaskManagerApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = Task.all
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    "<p>Params: #{params}</p> <p>Task params: #{params[:task]}</p>"
+  end
+
+  get '/tasks/:id' do
+    @task = Task.find(params[:id])
+    erb :show
+  end
+
+end
